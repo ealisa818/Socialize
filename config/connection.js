4e45fea00a763d127d1a2622e8df48990980cd94
ealisa818 <ealisa818@gmail.com>
2022-02-16 14:54:57 -0500

    final fix

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,22 @@
-const { connect, connection } = require('mongoose');
+// const { connect, connection } = require('mongoose');
 
-const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socializeDB';
+// const connectionString = 
+//     process.env.MONGODB_URI || 'mongodb://localhost:27017/socializeDB';
 
-connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+// module.exports = connection;
+
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://localhost:27017/aggregateDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-module.exports = connection;
+// Export connection 
+module.exports = mongoose.connection;
